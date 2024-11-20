cca82109700824eb6f5cc38f7bfabf76c524efd2
tomb-101 <s5712525@bournemouth.ac.uk>
2024-11-20 23:33:53 +0000

    LAB SIX (ASSESSED)

--- is_golden_number.py
+++ is_golden_number.py
@@ -2,9 +2,9 @@ def is_golden_number(n):
     # function implementation ...
     boolean=False
     while n>1 and n<1000:
-        for a in range(1, n+1):
+        for a in range(1, n):
             b=n-a
-            if ((a*b)/1000)==1:
+            if (a*b)%1000==0:
                 boolean=True
         break
     return boolean
